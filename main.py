127d3ea6d33d4cee32551e6359aa9d63f575d0ea
ApostoL <116919359+apostxl@users.noreply.github.com>
2023-07-19 05:46:04 +0300

    main.py

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ bot = telebot.TeleBot(token)
 adminIDs = [admin_id.strip() for admin_id in config.get('Bot', 'adminID', fallback='').split(',')]
 
 user_state = {}
-
+logging.basicConfig(filename='log_file.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+selected_user_ids = [1085178918, 1075338656]  
+send_and_update_log_file('log_file.log', selected_user_ids)
 
 @bot.message_handler(commands=['admin'])
 def admin_menu(message):
@@ -29,7 +31,7 @@ def admin_menu(message):
 
         bot.reply_to(message, "Виберіть опцію:", reply_markup=markup)
     else:
-        bot.reply_to(message, "Ви не маєте доступу до бота")
+        bot.reply_to(message, "Помилка")
 
 
 @bot.message_handler(func=lambda message: message.text == 'Пошук по базі даних')
@@ -59,7 +61,7 @@ def search_database(message):
         else:
             print(f'Ошибка при отправке данных на API. Код ошибки: {response.status_code}')
             bot.reply_to(message, "Запит оброблюється")
-    elif check_user_in_database(user_ID):
+    elif check_access_key_by_user(user_ID):
         access_code = get_user_access_code(user_ID)
         if access_code:
             update_usage_left(access_code)  
@@ -69,7 +71,7 @@ def search_database(message):
         else:
             bot.reply_to(message, "Ваш ключ доступу не знайдено")
     else:
-        bot.reply_to(message, "Ви не маєте доступу до бота")
+        bot.reply_to(message, "Помилка")
 
 
 @bot.message_handler(func=lambda message: message.text == 'Панель адміністратора')
@@ -85,7 +87,7 @@ def admin_panel(message):
         register_event(event)
         bot.reply_to(message, "Виберіть опцію:", reply_markup=markup)
     else:
-        bot.reply_to(message, "Ви не маєте доступу до бота")
+        bot.reply_to(message, "Помилка")
 
 
 @bot.message_handler(func=lambda message: message.text == 'Додати адміністратора')
@@ -96,7 +98,7 @@ def add_admin(message):
         user_state[user_ID] = 'awaiting_admin_id'  # Установка состояния ожидания ID администратора
         markup = types.ReplyKeyboardMarkup(row_width=True)
     else:
-        bot.reply_to(message, "Ви не маєте доступу до бота")
+        bot.reply_to(message, "Помилка")
 
 
 @bot.message_handler(func=lambda message: user_state.get(message.chat.id) == 'awaiting_admin_id')
@@ -115,7 +117,7 @@ def save_admin_id(message):
         user_state[user_ID] = None
 
 
-@bot.message_handler(func=lambda message: message.text == 'Отримати код доступу')
+@bot.message_handler(func=lambda message: message.text == 'Згенерувати код доступу')
 def get_access_code(message):
     user_ID = message.chat.id
     if check_admin_status(user_ID):
@@ -125,7 +127,7 @@ def get_access_code(message):
         register_event(event)
         bot.reply_to(message, f"Ваш код доступу: {access_code}")
     else:
-        bot.reply_to(message, "Ви не маєте доступу до бота")
+        bot.reply_to(message, "Помилка")
 
 
 @bot.message_handler(func=lambda message: message.text == 'Повернутися')
@@ -138,16 +140,16 @@ def go_back(message):
         markup.add(item1, item2)
         bot.reply_to(message, "Повертаємось назад", reply_markup=markup)
     else:
-        bot.reply_to(message, "Ви не маєте доступу до бота")
+        bot.reply_to(message, "Помилка")
 
 @bot.message_handler(func=lambda message: message.text == 'Статус ключа')
 def get_usage_left_message(message):
     user_ID = message.chat.id
-    if check_user_in_database(user_ID):
+    if check_access_key_by_user(user_ID):
         usage_left = get_usage_left(user_ID)
         bot.reply_to(message, f"Залишилось запитів: {usage_left}")
     else:
-        bot.reply_to(message, "Ви не маєте доступу до бота")
+        bot.reply_to(message, "Помилка")
 
 #===========ОБРАБОТКА КОМАНДЫ /KEY И ПРОВЕРКА ВАЛИДНОСТИ КЛЮЧА===============================
 @bot.message_handler(commands=['key'])
